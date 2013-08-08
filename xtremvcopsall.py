08160be1a6026ac543d37aa2f63a7c9122998e81
Matt Cowger <matt@cowger.us>
2013-08-07 21:17:20 -0700

    Added proper push for volume perf data. Cleaned up the code Better logging log to file (DEBUG) and screen (INFO)

--- xtremvcopsall.py
+++ xtremvcopsall.py
@@ -5,19 +5,22 @@ import requests
 import time
 import sys
 import logging
+import pprint
 
-logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
-#logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  {}")
+logFormatter = logging.Formatter("%(asctime)s [%(module)s:%(funcName)s] [%(levelname)-5.5s]  %(message)s")
+
 
 rootLogger = logging.getLogger()
-rootLogger.setLevel(logging.INFO)
+rootLogger.setLevel(logging.DEBUG)
 
 fileHandler = logging.FileHandler("xtremvcops.log")
 fileHandler.setFormatter(logFormatter)
+fileHandler.setLevel(logging.DEBUG)
 rootLogger.addHandler(fileHandler)
 
 consoleHandler = logging.StreamHandler()
 consoleHandler.setFormatter(logFormatter)
+consoleHandler.setLevel(logging.INFO)
 rootLogger.addHandler(consoleHandler)
         
 
@@ -51,17 +54,18 @@ def runPerfCommand(xmclicommand):
     command = " ".join(["xmcli","-u",xmcliuser,"-p",xmclipass,"-c",xmclicommand])
     stdin, stdout, stderr = ssh.exec_command(command)
     output = stdout.readlines()
-    rootLogger.info("returned from clicommand stdout %s",output)
+    rootLogger.debug("returned from clicommand stdout %s",output)
     rootLogger.debug("returned from clicommand stderr %s",stderr.readlines())
+    ssh.close()
     return output
     
 def cookSystemResults(output):
-    rootLogger.info("processing command as system result %s",output)
+    rootLogger.debug("processing command as system result %s",output)
     alldata = []
     data = {'arrayname': "", 'index': "", 'WriteBW': "", 'WriteIO': "", 'ReadBW': "", 'ReadIO': "", 'BW': "", 'IOPs': "", 'TotalWriteIO': "", 'TotalReadIO': ""}
     for line in output[1:]:
         data['arrayname'],data['index'],data['WriteBW'],data['WriteIO'],data['ReadBW'],data['ReadIO'],data['BW'],data['IOPs'],data['TotalWriteIO'],data['TotalReadIO'] = line.split()
-        rootLogger.info("appending to alldata for this run %s",data)
+        rootLogger.debug("appending to alldata for this run %s",data)
         alldata.append(data)
     return alldata[0]
     
@@ -72,9 +76,11 @@ def getSystemName():
     return systemname
 
 def collectPerf():
-    systemFirstLine = buildSystemFirstLine(getSystemName())
+    systemName = getSystemName()
+    systemFirstLine = buildSystemFirstLine(systemName)
     systemMetricLines = buildSystemMetricLines(cookSystemResults(runPerfCommand("show-systems-performance")))
     postMetrics(systemFirstLine,systemMetricLines)
+    buildVolumesMetricLines(systemName)
 
 def buildSystemFirstLine(systemName):
     resourceName = "-".join(["XtremIO",systemName])
@@ -94,7 +100,6 @@ def buildSystemMetricLines(cookedperfdata):
     alarmLevel = "0"
     alarmMessage = "NA"
     date = str(int(time.time()*1000))
-    print cookedperfdata
     for metricName in cookedperfdata:
         if (metricName != 'arrayname') & (metricName != 'index'):
             value = str(cookedperfdata[metricName])
@@ -102,21 +107,56 @@ def buildSystemMetricLines(cookedperfdata):
             allLines.append(metricLine)
     return allLines
     
+def buildVolumeFirstLine(systemName,volumeName):
+    resourceName = "-".join(["XtremIO",systemName,volumeName])
+    adapterKindKey = "XtremIO-Adapter"
+    resourceKindKey = "XtremIO-Volume"
+    identifiers = ""
+    resourceDescription = "-".join(["XtremIO","Array",systemName,volumeName])
+    monitoringInterval = '1'
+    storeOnly = 'false'
+    sourceAdapter = 'XtremIO-Alpha-Demo-Adapter'
+    disableResourceCreation = 'false'
+    firstLine = ",".join([resourceName,adapterKindKey,resourceKindKey,identifiers,resourceDescription,monitoringInterval,storeOnly,sourceAdapter,disableResourceCreation])
+    return firstLine
+
+def buildVolumesMetricLines(systemName):
+    alarmLevel = "0"
+    alarmMessage = "NA"
+    date = str(int(time.time()*1000))
+    rawVolumeMetrics = runPerfCommand("show-volumes-performance")
+    rootLogger.debug("processing command as volumes result %s",rawVolumeMetrics)
+    for volume in rawVolumeMetrics[1:]:
+        data = {'volumeName': "", 'index': "", 'WriteBW': "", 'WriteIO': "", 'ReadBW': "", 'ReadIO': "", 'BW': "", 'IOPs': "", 'TotalWriteIO': "", 'TotalReadIO': ""}
+        data['volumeName'],data['index'],data['WriteBW'],data['WriteIO'],data['ReadBW'],data['ReadIO'],data['BW'],data['IOPs'],data['TotalWriteIO'],data['TotalReadIO'] = volume.strip().split()
+        firstLine = buildVolumeFirstLine(systemName,data['volumeName'])
+        volumeMetrics = []
+        for metricName in data:
+            if (metricName != 'volumeName') & (metricName != 'index'):
+                value = str(data[metricName])
+                metricLine = ",".join([metricName,alarmLevel,alarmMessage,date,value])
+                volumeMetrics.append(metricLine)
+        postMetrics(firstLine,volumeMetrics)
+        
+        
+    
 def postMetrics(firstLine,metricLines):
-    rootLogger.info("entering vcops post for firstline %s",firstLine)
+    rootLogger.info("entering vcops post for resource name: %s",firstLine.split(',')[0])
     allMetrics = "\n".join(metricLines)
     postData = "\n".join([firstLine,allMetrics])
     
-    rootLogger.info("final post data \n\n--------------%s\n--------------\n",postData)
+    rootLogger.debug("final post data \n\n--------------\n%s\n--------------\n",postData)
     r = requests.post(vcopsurl, postData, auth=(vcopsuser, vcopspass), verify=False)
     r.raise_for_status()
     rootLogger.info("post result %s",r.status_code)
 
 
-collectPerf()
 
-
-        
+if __name__ == '__main__':
+    while True:
+        collectPerf()
+        rootLogger.info("sleeping 60s before next run")
+        time.sleep(60)
 
         
     
